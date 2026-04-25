3d0f4e59ff254cdf2b24b73448ddc0e1b87fde53
NaumVlavceski <nvlavceski542@gmail.com>
2026-04-25 23:04:24 +0200

    SCRUM-21 Design and create Service entity & migration

---
+++ Backend/src/main/java/com/example/backend/model/Service.java
@@ -0,0 +1,27 @@
+package com.example.backend.model;
+
+import jakarta.persistence.*;
+import jakarta.validation.constraints.DecimalMin;
+import jakarta.validation.constraints.Digits;
+import lombok.Data;
+
+import java.math.BigDecimal;
+
+@Entity(name = "")
+@Data
+public class Services {
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+
+    private String name;
+    private String description;
+    private int duration;
+    @Digits(integer = 10, fraction = 2)
+    private BigDecimal price;
+    private boolean isActive;
+    @ManyToOne
+    @JoinColumn(name = "business_id")
+    private BusinessUser business;
+
+}
